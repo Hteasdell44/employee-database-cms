b38669b6f09945d0737990c351c30e8cfe08cc51
hteasdell44 <hteasdell44@gmail.com>
2023-04-20 00:00:56 -0400

    Configured All CRUD Operations To Alter The DB

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,5 +15,9 @@ VALUES (1, 'WEB DEVELOPER', 900000, 1),
        (4, 'PRODUCT MANAGER', 100000, 4),
        (5, 'CEO', 1000000, 6);
 
-
-
+INSERT INTO employee
+VALUES (1, 'Jessica', 'Jones', 2, 7),
+       (2, 'Arnold', 'Hayes', 4, 2),
+       (3, 'Bryson', 'Donaldson', 5, 1),
+       (4, 'Kari', 'Hunt', 3, 4);
+           
